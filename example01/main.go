46350280a8c8a67596f25465294561ee0866a71f
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-03-08 11:35:07 +0800

    update
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example01/main.go
+++ example01/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	a := 1
 	fmt.Println("一天就學會 Go 語言")
+	fmt.Println(HelloWorld())
+
+	if a >= 1 {
+		fmt.Println("a >= 1")
+	}
+}
+
+func HelloWorld() string {
+	return fmt.Sprint("一天就學會 Go 語言")
 }
