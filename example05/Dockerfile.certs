c3993339d739370cbda017ce918001361b320886
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-03-15 11:56:17 +0800

    update docker section.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ example05/Dockerfile.certs
@@ -0,0 +1,5 @@
+FROM centurylink/ca-certs
+
+ADD main /
+
+CMD ["/main"]
--- example05/main.go
+++ example05/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hi, 一天學會 golang")
+
+	// try changing the value of this url
+	res, err := http.Get("https://golang.org")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res.Status)
 }
