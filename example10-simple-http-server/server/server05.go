f990cb8fc987b5cfdff197ad13e500194defef5c
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-04-05 09:16:18 +0800

    feat: add example for integrate with letsencrypt (#1)

---
+++ example10-simple-http-server/server/server05.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Ping handler
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	m := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("example1.com", "example2.com"),
+		Cache:      autocert.DirCache("/var/www/.cache"),
+	}
+
+	s := &http.Server{
+		Addr:      ":https",
+		TLSConfig: &tls.Config{GetCertificate: m.GetCertificate},
+		Handler:   r,
+	}
+	s.ListenAndServeTLS("", "")
+}
