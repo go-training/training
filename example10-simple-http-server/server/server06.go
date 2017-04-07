7fb9fe549a9138b462deac3b9ec9a99c00c022f2
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-04-07 09:08:08 +0800

    feat: one line code to integrate LetsEncrypt

---
+++ example10-simple-http-server/server/server06.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Ping handler
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	log.Fatal(http.Serve(autocert.NewListener("example1.com", "example2.com"), r))
+}
