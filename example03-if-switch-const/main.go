a7fa55dc3f6f256dc425fe24363e3b18180f3154
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-05-18 15:32:45 +0800

    add slice
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example03-if-switch-const/main.go
+++ example03-if-switch-const/main.go
@@ -49,4 +49,21 @@ func main() {
 	case a > 200:
 		fmt.Println("a is false")
 	}
+
+	stringSlice1 := []string{"1", "2", "3", "4"}
+	fmt.Println(stringSlice1[:2])
+	fmt.Println(stringSlice1[2:])
+
+	stringSlice2 := make([]string, 5, 10)
+	copy(stringSlice2, stringSlice1)
+	fmt.Println(stringSlice2[0])
+
+	stringSlice2 = append(stringSlice2, "5", "6")
+	fmt.Println(stringSlice2)
+	fmt.Println(stringSlice2[5])
+	fmt.Println(stringSlice2[6])
+	stringSlice2 = append(stringSlice2, []string{"7", "8"}...)
+	fmt.Println(stringSlice2)
+	fmt.Println(stringSlice2[7])
+	fmt.Println(stringSlice2[8])
 }
