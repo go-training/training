771a259b6e994299b2e49022b7fd846d0240a422
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-03-18 10:24:07 +0800

    fix import path
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example02-golang-package/main.go
+++ example02-golang-package/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/go-training/training/example02/hello"
+	"github.com/go-training/training/example02-golang-package/controller"
 )
 
 func main() {
 	fmt.Println("一天就學會 Go 語言")
 
-	hi := hello.HelloWorld("appleboy")
+	hi := controller.HelloWorld("appleboy")
 	fmt.Println(hi)
 }
