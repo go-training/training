4518fb198c0c5e6f43eb7afad910a24c3b64d7ef
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-03-25 14:30:38 +0800

    fix golint
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example07-errors-handler/2.custom-error/error.go
+++ example07-errors-handler/2.custom-error/error.go
@@ -13,6 +13,7 @@ func (e ErrUserNameExist) Error() string {
 	return fmt.Sprintf("username %s already exist", e.UserName)
 }
 
+// IsErrUserNameExist check error type
 func IsErrUserNameExist(err error) bool {
 	_, ok := err.(ErrUserNameExist)
 	return ok
