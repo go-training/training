c6a162fbc9a6f90c0d777d4f0fc4bebb4d884de5
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-04-07 11:28:56 +0800

    feat: add pinger.

--- example09-command-line-tool/flag/main.go
+++ example09-command-line-tool/flag/main.go
@@ -14,14 +14,31 @@ func HelloWorld() string {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("handler http request")
 	fmt.Fprintf(w, "%s, I love %s!", HelloWorld(), r.URL.Path[1:])
+}
+
+// handles pinging the endpoint and returns an error if the
+// agent is in an unhealthy state.
+func pinger(port string) error {
+	resp, err := http.Get("http://localhost:" + port)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("server returned non-200 status code")
+	}
+	return nil
 }
 
 func main() {
 	var showVersion bool
+	var ping bool
 	var port string
 	flag.BoolVar(&showVersion, "version", false, "Print version information.")
 	flag.BoolVar(&showVersion, "v", false, "Print version information.")
+	flag.BoolVar(&ping, "ping", false, "check server live.")
 	flag.StringVar(&port, "port", "8080", "server port")
 	flag.StringVar(&port, "p", "8080", "server port")
 	flag.Parse()
@@ -36,6 +53,14 @@ func main() {
 		port = p
 	}
 	flag.Parse()
+
+	if ping {
+		if err := pinger(port); err != nil {
+			log.Printf("ping server error: %v\n", err)
+		}
+		return
+	}
+
 	http.HandleFunc("/", handler)
 	log.Println("http server run on " + port + " port")
 	log.Fatal(http.ListenAndServe(":"+port, nil))
