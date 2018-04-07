ba978505757bd907c229a9746e49b432f098e555
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-04-07 11:23:50 +0800

    udpate flag command line.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example09-command-line-tool/flag/main.go
+++ example09-command-line-tool/flag/main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
+// HelloWorld for hello world
+func HelloWorld() string {
+	return "Hello World, golang workshop!"
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s, I love %s!", HelloWorld(), r.URL.Path[1:])
+}
+
 func main() {
 	var showVersion bool
+	var port string
 	flag.BoolVar(&showVersion, "version", false, "Print version information.")
 	flag.BoolVar(&showVersion, "v", false, "Print version information.")
+	flag.StringVar(&port, "port", "8080", "server port")
+	flag.StringVar(&port, "p", "8080", "server port")
 	flag.Parse()
 
 	// Show version and exit
@@ -18,5 +32,11 @@ func main() {
 		os.Exit(0)
 	}
 
-	fmt.Println("try -h to get more information")
+	if p, ok := os.LookupEnv("PORT"); ok {
+		port = p
+	}
+	flag.Parse()
+	http.HandleFunc("/", handler)
+	log.Println("http server run on " + port + " port")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
