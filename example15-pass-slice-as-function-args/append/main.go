990af0ec6605e1e5f9ce239cc9380d79d80ddbce
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-04-15 21:23:09 +0800

    update slice example.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example15-pass-slice-as-function-args/append/main.go
+++ example15-pass-slice-as-function-args/append/main.go
@@ -13,4 +13,15 @@ func main() {
 	fmt.Println("before foo:", foo)
 	addValue(foo)
 	fmt.Println("after foo:", foo)
+	bar := foo[:1]
+	fmt.Println("bar:", bar)
+	s1 := append(bar, "c")
+	fmt.Println("foo:", foo)
+	fmt.Println("s1:", s1)
+	s2 := append(bar, "d")
+	fmt.Println("foo:", foo)
+	fmt.Println("s2:", s2)
+	s3 := append(bar, "e", "f")
+	fmt.Println("foo:", foo)
+	fmt.Println("s3:", s3)
 }
