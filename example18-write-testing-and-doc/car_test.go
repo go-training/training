2ab697ec933f33ac7eed7bbe705d4a19b656acda
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-05-13 15:07:55 +0800

    feat: add Simple testing with testify tool

--- example18-write-testing-and-doc/car_test.go
+++ example18-write-testing-and-doc/car_test.go
@@ -1,14 +1,26 @@
 package car
 
-import "testing"
+import (
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+)
+
+// Simple testing what different between Fatal and Error
 func TestNew(t *testing.T) {
 	c, err := New("", 100)
-	if err == nil {
+	if err != nil {
 		t.Fatal("got errors:", err)
 	}
 
 	if c == nil {
 		t.Error("car should be nil")
 	}
+}
+
+// Simple testing with testify tool
+func TestNewWithAssert(t *testing.T) {
+	c, err := New("", 100)
+	assert.NotNil(t, err)
+	assert.Nil(t, c)
 }
