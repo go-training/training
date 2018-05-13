a31db6451184187b9dee239c18d5663d2d225700
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-05-13 15:14:28 +0800

    add unit testing.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example18-write-testing-and-doc/car_test.go
+++ example18-write-testing-and-doc/car_test.go
@@ -24,3 +24,55 @@ func TestNewWithAssert(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Nil(t, c)
 }
+
+// Testing
+func TestCar_SetName(t *testing.T) {
+	type fields struct {
+		Name  string
+		Price float32
+	}
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{
+			name: "input empty name",
+			fields: fields{
+				Name:  "foo",
+				Price: 100,
+			},
+			args: args{
+				name: "",
+			},
+			want: "foo",
+		},
+		{
+			name: "input name",
+			fields: fields{
+				Name:  "foo",
+				Price: 100,
+			},
+			args: args{
+				name: "bar",
+			},
+			want: "bar",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			c := &Car{
+				Name:  tt.fields.Name,
+				Price: tt.fields.Price,
+			}
+			if got := c.SetName(tt.args.name); got != tt.want {
+				t.Errorf("Car.SetName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
