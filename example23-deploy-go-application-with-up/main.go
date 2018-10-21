3c7a2b95512a394e8d820f59b0c2ed2c43d53d65
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-10-21 14:53:12 +0800

    initial deploy with up
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ example23-deploy-go-application-with-up/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := ":" + os.Getenv("PORT")
+	stage := os.Getenv("UP_STAGE")
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong " + stage,
+		})
+	})
+	r.Run(port)
+}
