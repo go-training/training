ed9659a2a27cae713a0e040c29d91270f846299a
joeyghb <kuang7156@gmai.com>
2018-11-04 14:31:41 +0800

    update example02 , hello pgluffy

--- example02-golang-package/main.go
+++ example02-golang-package/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/go-training/training/example02-golang-package/controller"
+	"golang_training/example02-golang-package/controller"
 )
 
 func main() {
 	fmt.Println("一天就學會 Go 語言")
 
-	hi := controller.HelloWorld("appleboy")
+	hi := controller.HelloWorld("pgluffy")
 	fmt.Println(hi)
 }
