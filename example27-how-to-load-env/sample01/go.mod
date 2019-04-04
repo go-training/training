b6b42118c5908111f27bbbec643c9bd5ebbbb7c7
Bo-Yi Wu <appleboy.tw@gmail.com>
2019-04-04 11:02:37 +0800

    feat: add how to load env

---
+++ example27-how-to-load-env/sample01/go.mod
@@ -0,0 +1,5 @@
+module github.com/appleboy/dotenv
+
+go 1.12
+
+require github.com/joho/godotenv v1.3.0 // indirect
---
+++ example27-how-to-load-env/sample01/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	s3Bucket := os.Getenv("S3_BUCKET")
+	secretKey := os.Getenv("SECRET_KEY")
+
+	fmt.Println(s3Bucket)
+	fmt.Println(secretKey)
+}
