f97c1a0fb945ff6df465e37089a5f3cf81e2b7d3
Bo-Yi Wu <appleboy.tw@gmail.com>
2019-04-04 08:45:01 +0800

    chore: update

--- example06-go-concurrency/buffer-vs-unbuffer/unbuffer/unbuffer.go
+++ example06-go-concurrency/buffer-vs-unbuffer/unbuffer/unbuffer.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	c := make(chan bool)
 	go func() {
 		fmt.Println("GO GO GO")
-		<-c
+		c <- true
 	}()
-	c <- true
-	time.Sleep(1 * time.Second)
+	<-c
 }
