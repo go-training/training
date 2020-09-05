08cfaf588caa3fd69978cd7a6053440003b19341
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-09-05 18:46:27 +0800

    chore: add example38

---
+++ example38-concurrency-is-still-not-easy/question/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	const concurrencyProcesses = 10 // limit the maximum number of concurrent reading process tasks
+	const jobCount = 100
+
+	var wg sync.WaitGroup
+	wg.Add(jobCount)
+	found := make(chan int)
+	limitCh := make(chan struct{}, concurrencyProcesses)
+
+	for i := 0; i < jobCount; i++ {
+		limitCh <- struct{}{}
+		go func(val int) {
+			defer func() {
+				wg.Done()
+				<-limitCh
+			}()
+			waitTime := rand.Int31n(1000)
+			fmt.Println("wait time:", waitTime, "millisecond")
+			time.Sleep(time.Duration(waitTime) * time.Millisecond)
+			found <- val
+		}(i)
+	}
+	go func() {
+		wg.Wait()
+		close(found)
+	}()
+	var results []int
+	for p := range found {
+		fmt.Println("Finished job:", p)
+		results = append(results, p)
+	}
+
+	fmt.Println("result:", results)
+}
