5e7b590551256299ded075ebe419ed8a6a2a7ed3
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-09-05 19:59:12 +0800

    chore(example38): add solution02

---
+++ example38-concurrency-is-still-not-easy/answer02/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	const concurrencyProcesses = 10 // limit the maximum number of concurrent reading process tasks
+	const jobCount = 100
+
+	var wg sync.WaitGroup
+	wg.Add(jobCount)
+	found := make(chan int)
+	queue := make(chan int)
+
+	go func(queue chan<- int) {
+		for i := 0; i < jobCount; i++ {
+			queue <- i
+		}
+		close(queue)
+	}(queue)
+
+	for i := 0; i < concurrencyProcesses; i++ {
+		go func(queue <-chan int, found chan<- int) {
+			for val := range queue {
+				defer wg.Done()
+				waitTime := rand.Int31n(1000)
+				fmt.Println("wait time:", waitTime, "millisecond")
+				time.Sleep(time.Duration(waitTime) * time.Millisecond)
+				found <- val
+			}
+		}(queue, found)
+	}
+	go func() {
+		wg.Wait()
+		close(found)
+	}()
+	var results []int
+	for p := range found {
+		fmt.Println("Finished job:", p)
+		results = append(results, p)
+	}
+
+	fmt.Println("result:", results)
+}
