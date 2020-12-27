1ad1126f637c0693e196ef7fe418c71428589d32
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-12-27 15:02:15 +0800

    chore: support Embedding Files in go 1.16

--- example40-embedding-files/example02/main.go
+++ example40-embedding-files/example02/main.go
@@ -13,12 +13,12 @@ func main() {
 	var f embed.FS
 
 	router := gin.Default()
+	templ := template.Must(template.New("").ParseFS(f, "templates/*.tmpl", "templates/foo/*.tmpl"))
+	router.SetHTMLTemplate(templ)
 
 	// example: /public/assets/images/example.png
 	router.StaticFS("/public", http.FS(f))
 
-	templ := template.Must(template.New("").ParseFS(f, "templates/*.tmpl", "templates/foo/*.tmpl"))
-	router.SetHTMLTemplate(templ)
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Main website",
