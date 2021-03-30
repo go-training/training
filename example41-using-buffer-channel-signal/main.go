323e53e1407889ba6bbf2f6664293d563169db42
Bo-Yi Wu <appleboy.tw@gmail.com>
2021-03-30 09:25:37 +0800

    chore(example41): We must use a buffered channel or risk missing the signal

---
+++ example41-using-buffer-channel-signal/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	// Set up channel on which to send signal notifications.
+	// We must use a buffered channel or risk missing the signal
+	// if we're not ready to receive when the signal is sent.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	// Block until a signal is received.
+	s := <-c
+	fmt.Println("Got signal:", s)
+}
