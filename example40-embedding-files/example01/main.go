58be4931565fe4f9d80b4e4763a63b5743474b94
Bo-Yi Wu <appleboy.tw@gmail.com>
2021-07-21 08:56:34 +0800

    chore(example40): go:embed cannot apply to var inside func
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example40-embedding-files/example01/main.go
+++ example40-embedding-files/example01/main.go
@@ -4,17 +4,18 @@ import (
 	"embed"
 )
 
+//go:embed hello.txt
+var s string
+
+//go:embed hello.txt
+var b []byte
+
+//go:embed hello.txt
+var f embed.FS
+
 func main() {
-	//go:embed hello.txt
-	var s string
 	print(s)
-
-	//go:embed hello.txt
-	var b []byte
 	print(string(b))
-
-	//go:embed hello.txt
-	var f embed.FS
 	data, _ := f.ReadFile("hello.txt")
 	print(string(data))
 }
