e30768b21b0b9b36d0d83dfe1fa2a56fae2de62d
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-02-12 08:11:04 +0800

    chore(example45): Generics example 02
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example45-go-1.18-generics/example02/main.go
+++ example45-go-1.18-generics/example02/main.go
@@ -6,8 +6,18 @@ type age interface {
 	int8 | int16 | int32 | int64 | float32 | float64
 }
 
-func newGenerics[num age](s num) {
-	val := float64(s) + 1
+func newGenerics[num age](s1 num) {
+	val := float64(s1) + 1
+	fmt.Println(val)
+}
+
+func total[num age](s1, s2 num) {
+	val := float64(s1) + float64(s2)
+	fmt.Println(val)
+}
+
+func summary[num1, num2 age](s1 num1, s2 num2) {
+	val := float64(s1) + float64(s2)
 	fmt.Println(val)
 }
 
@@ -19,4 +29,14 @@ func main() {
 
 	newGenerics(sum1)
 	newGenerics(sum2)
+
+	var sum3 float64 = 28
+	var sum4 float64 = 29.5
+
+	total(sum3, sum4)
+
+	var sum5 int64 = 28
+	var sum6 float64 = 29.5
+
+	summary(sum5, sum6)
 }
