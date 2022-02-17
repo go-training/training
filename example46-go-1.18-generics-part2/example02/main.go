f56fcb54b4128167f1681b7dd41f741b346ccd10
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-02-17 15:21:05 +0800

    chore(example46): custom interface constrain
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example46-go-1.18-generics-part2/example02/main.go
+++ example46-go-1.18-generics-part2/example02/main.go
@@ -22,11 +22,52 @@ func (c car) String() string {
 	return fmt.Sprintf("%d", c.price)
 }
 
+type plusser interface {
+	Plus(string) string
+}
+
+func cconcatTo[S stringer, P plusser](s []S, p []P) []string {
+	r := make([]string, len(s))
+	for i, v := range s {
+		r[i] = p[i].Plus(v.String())
+	}
+	return r
+}
+
+type foo struct {
+	name string
+}
+
+func (c foo) String() string {
+	return c.name
+}
+
+type plus map[string]string
+
+func (p plus) Plus(k string) string {
+	if v, ok := p[k]; ok {
+		return v
+	}
+	return ""
+}
+
 func main() {
+	// exmaple01
 	val := concat([]stringer{
 		car{price: 1},
 		car{price: 2},
 	})
 
 	fmt.Println(val)
+
+	// example02
+	p := plus{
+		"a": "100",
+		"b": "200",
+	}
+
+	fmt.Println(cconcatTo([]stringer{
+		foo{name: "a"},
+		foo{name: "b"},
+	}, []plusser{p, p}))
 }
