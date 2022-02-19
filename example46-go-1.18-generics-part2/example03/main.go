c09b22c189f277a6299c83e1411e0df9a5a550e0
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-02-19 11:15:41 +0800

    chore(example64): show constraints package
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example46-go-1.18-generics-part2/example03/main.go
+++ example46-go-1.18-generics-part2/example03/main.go
@@ -1,12 +1,39 @@
 package main
 
+// Comparable constraint
+
 import (
+	"constraints"
 	"errors"
 	"fmt"
 )
 
 func indexOf[T comparable](s []T, x T) (int, error) {
 	for i, v := range s {
+		// v and x are type T, which has the comparable
+		// constraint, so we can use == here.
+		if v == x {
+			return i, nil
+		}
+	}
+	return 0, errors.New("not found")
+}
+
+func indexOfInteger[T constraints.Integer](s []T, x T) (int, error) {
+	for i, v := range s {
+		// v and x are type T, which has the comparable
+		// constraint, so we can use == here.
+		if v == x {
+			return i, nil
+		}
+	}
+	return 0, errors.New("not found")
+}
+
+func indexOfFloat[T constraints.Float](s []T, x T) (int, error) {
+	for i, v := range s {
+		// v and x are type T, which has the comparable
+		// constraint, so we can use == here.
 		if v == x {
 			return i, nil
 		}
@@ -17,5 +44,13 @@ func indexOf[T comparable](s []T, x T) (int, error) {
 func main() {
 	i, err := indexOf([]string{"apple", "banana", "pear"}, "banana")
 	fmt.Println(i, err)
+	i, err = indexOf([]int{1, 2, 3}, 3)
+	fmt.Println(i, err)
 	// prints 1 <nil>
+
+	i, err = indexOfInteger([]int{1, 2, 3}, 3)
+	fmt.Println(i, err)
+
+	i, err = indexOfFloat([]float64{1.1, 2.2, 3.3, 4.4}, 4.4)
+	fmt.Println(i, err)
 }
