5475e9adce53ee44659d0db5d6aa3327fecaef85
Bo-Yi.Wu <appleboy.tw@gmail.com>
2022-11-12 12:35:56 +0800

    chore(example50): initial answer format
    Signed-off-by: Bo-Yi.Wu <appleboy.tw@gmail.com>

--- example50-consumer-producer-pattern/answer/poller.go
+++ example50-consumer-producer-pattern/answer/poller.go
@@ -1,4 +1,4 @@
-package main
+package answer
 
 import (
 	"context"
@@ -6,19 +6,17 @@ import (
 	"time"
 )
 
-func main() {
-	producer := &poller{
+func NewPoller() *Poller {
+	return &Poller{
 		routineGroup: newRoutineGroup(),
 	}
-
-	producer.Poll(context.Background(), 10)
 }
 
-type poller struct {
+type Poller struct {
 	routineGroup *routineGroup
 }
 
-func (p *poller) Poll(ctx context.Context, n int) error {
+func (p *Poller) Poll(ctx context.Context, n int) error {
 	for i := 0; i < n; i++ {
 		func(i int) {
 			p.routineGroup.Run(func() {
@@ -37,22 +35,24 @@ func (p *poller) Poll(ctx context.Context, n int) error {
 	return nil
 }
 
-func (p *poller) poll(ctx context.Context, n int) error {
+func (p *Poller) poll(ctx context.Context, n int) error {
+	log.Printf("gorutine %02d: fetch task\n", n)
 	task, err := p.fetch(ctx)
 	if err != nil {
 		return nil
 	}
 
+	time.Sleep(200 * time.Millisecond)
 	log.Printf("gorutine %02d: execute task\n", n)
 	return p.execute(ctx, task)
 }
 
-func (p *poller) fetch(ctx context.Context) (string, error) {
+func (p *Poller) fetch(ctx context.Context) (string, error) {
 	// connect database or other service
 	time.Sleep(400 * time.Millisecond)
 	return "foobar", nil
 }
 
-func (p *poller) execute(ctx context.Context, task string) error {
+func (p *Poller) execute(ctx context.Context, task string) error {
 	return nil
 }
--- example50-consumer-producer-pattern/answer/thread.go
+++ example50-consumer-producer-pattern/answer/thread.go
@@ -1,4 +1,4 @@
-package main
+package answer
 
 import "sync"
 
