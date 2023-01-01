3c8dfba61162c9a7a74c076fb1c099d21c9188b1
Bo-Yi.Wu <appleboy.tw@gmail.com>
2023-01-01 16:23:52 +0800

    chore(example51): re-define subsciber map
    Signed-off-by: Bo-Yi.Wu <appleboy.tw@gmail.com>

--- example51-canceler/schedule/canceler.go
+++ example51-canceler/schedule/canceler.go
@@ -9,7 +9,7 @@ import (
 type canceler struct {
 	sync.Mutex
 
-	subsciber map[chan struct{}]string
+	subsciber map[string]chan struct{}
 	cancelled map[string]time.Time
 }
 
@@ -18,11 +18,8 @@ func (c *canceler) Cancel(ctx context.Context, id string) error {
 	c.Lock()
 	defer c.Unlock()
 	c.cancelled[id] = time.Now().Add(5 * time.Minute)
-	for subsciber, target := range c.subsciber {
-		if id == target {
-			close(subsciber)
-			return nil
-		}
+	if sub, ok := c.subsciber[id]; ok {
+		close(sub)
 	}
 	c.clear()
 	return nil
@@ -32,12 +29,12 @@ func (c *canceler) Cancel(ctx context.Context, id string) error {
 func (c *canceler) Cancelled(ctx context.Context, id string) (bool, error) {
 	subsciber := make(chan struct{})
 	c.Lock()
-	c.subsciber[subsciber] = id
+	c.subsciber[id] = subsciber
 	c.Unlock()
 
 	defer func() {
 		c.Lock()
-		delete(c.subsciber, subsciber)
+		delete(c.subsciber, id)
 		c.Unlock()
 	}()
 
@@ -67,7 +64,7 @@ func (c *canceler) clear() {
 
 func newCanceler() *canceler {
 	return &canceler{
-		subsciber: make(map[chan struct{}]string),
+		subsciber: make(map[string]chan struct{}),
 		cancelled: make(map[string]time.Time),
 	}
 }
