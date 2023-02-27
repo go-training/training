f85ca6a73b58c983a15ec6f30f95fa5f0d754ec2
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-02-27 09:09:30 +0800

    chore(example53): add foobar handler

--- example53-container-base-image-updates/main.go
+++ example53-container-base-image-updates/main.go
@@ -14,6 +14,11 @@ func main() {
 		})
 	})
 	r.GET("/foobar", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "foobar",
+		})
+	})
+	r.GET("/", func(c *gin.Context) {
 		c.AbortWithStatus(http.StatusOK)
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
