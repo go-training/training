b25e4b6ecd0c4bd884bf2fb0532c07d275531519
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-02-27 08:57:47 +0800

    chore(example53): add / handler

--- example53-container-base-image-updates/main.go
+++ example53-container-base-image-updates/main.go
@@ -13,5 +13,8 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.GET("/", func(c *gin.Context) {
+		c.AbortWithStatus(http.StatusOK)
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
