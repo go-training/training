48c410886e9ce2da65a5b3888fe4f5baa2ce32ad
appleboy <appleboy.tw@gmail.com>
2024-01-14 13:56:49 +0800

    build: add build instructions and web server implementation
    - Add a new file `Makefile` with build instructions
    - Add a new file `bin/web` with executable permissions
    - Add a new file `go.mod` with module information
    - Add a new file `go.sum` with module checksums
    - Add a new file `main.go` with a simple web server implementation
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

---
+++ example54-hot-reload-for-development/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+func main() {
+	// write simple web server using http package
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+	slog.Info("Server started at port 8080")
+	// support grace shutdown
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
