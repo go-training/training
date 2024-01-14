227a66e218b1689f8f7e381b9a903cee3b625345
appleboy <appleboy.tw@gmail.com>
2024-01-14 14:01:11 +0800

    feat: add gitignore, hot-reloading, and new route
    - Add `.gitignore` file with `bin` and `tmp` directories
    - Add `air` command to the Makefile for hot-reloading
    - Delete `bin/web` file
    - Add `/hello` route to `main.go`
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- example54-hot-reload-for-development/main.go
+++ example54-hot-reload-for-development/main.go
@@ -10,6 +10,11 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
 	slog.Info("Server started at port 8080")
 	// support grace shutdown
 	if err := http.ListenAndServe(":8080", nil); err != nil {
