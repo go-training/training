4d5073f90bf3563041a744acddd604ef0a0917be
Bo-Yi Wu <appleboy.tw@gmail.com>
2024-02-03 20:25:50 +0800

    refactor: refactor GetArticle and Improve Logging
    - Store the result of `GetArticle` in `data` and log it along with the request
    - Refactor `GetArticle` method to take `req` instead of `worker` as the first argument
    - Update logging statements to include `req` instead of `worker` in `cache hit` and `missing cache` logs
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example55-cache-hotspot-invalid/main.go
+++ example55-cache-hotspot-invalid/main.go
@@ -15,7 +15,8 @@ func main() {
 	for i := 0; i < 5; i++ {
 		go func(req int) {
 			defer wg.Done()
-			_ = db.GetArticle(req, 1)
+			data := db.GetArticle(req, 1)
+			slog.Info("data", "data", data, "req", req)
 		}(i)
 	}
 	wg.Wait()
@@ -25,10 +26,10 @@ type DB struct {
 	cache *Cache
 }
 
-func (db *DB) GetArticle(worker int, id int) *Article {
+func (db *DB) GetArticle(req int, id int) *Article {
 	data := db.cache.Get(id)
 	if data != nil {
-		slog.Info("cache hit", "id", id, "worker", worker)
+		slog.Info("cache hit", "id", id, "req", req)
 		return data
 	}
 
@@ -37,6 +38,6 @@ func (db *DB) GetArticle(worker int, id int) *Article {
 		Content: "FooBar",
 	}
 	db.cache.Set(id, data)
-	slog.Info("missing cache", "id", id, "worker", worker)
+	slog.Info("missing cache", "id", id, "req", req)
 	return data
 }
